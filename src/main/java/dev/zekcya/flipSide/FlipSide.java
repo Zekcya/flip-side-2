1b9df37e92b67925e2a0a53ff438771600786f10
Zekcya <118208720+Zekcya@users.noreply.github.com>
2025-11-02 15:26:09 -0600

    Flipside2 -> FlipSide ver. 2.0

--- src/main/java/dev/zekcya/flipSide/FlipSide.java
+++ src/main/java/dev/zekcya/flipSide/FlipSide.java
@@ -1,8 +1,8 @@
-package dev.zekcya.flipSide2;
+package dev.zekcya.flipSide;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class FlipSide2 extends JavaPlugin {
+public final class FlipSide extends JavaPlugin {
 
     @Override
     public void onEnable() {
